f2abaac889f42f58d2270f857943586d842ecaeb
Elad Iwanir <13205761+eladiw@users.noreply.github.com>
2020-03-22 14:24:50 +0200

    lint

--- presidio-analyzer/presidio_analyzer/predefined_recognizers/il_id_recognizer.py
+++ presidio-analyzer/presidio_analyzer/predefined_recognizers/il_id_recognizer.py
@@ -2,7 +2,7 @@ from presidio_analyzer import Pattern
 from presidio_analyzer import PatternRecognizer
 # pylint: disable=line-too-long,abstract-method
 
-ISRAELI_ID_REGEX = '[0-9]{7,10}' # \u05EA\.\u05D6
+ISRAELI_ID_REGEX = '[0-9]{7,10}'  # \u05EA\.\u05D6
 
 # Todo: fix context for unsupported languages
 CONTEXT = [
